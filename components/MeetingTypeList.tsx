5ff2797ebad3d6fdfff1fbf9ddbc9effac4101fd
atharvk47 <atharv.kurlapkar47@gmail.com>
2024-08-30 00:29:22 +0530

    ft. MeetingModal created, Instant meeting dialog created using ShadCN

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,19 +3,22 @@ import Image from "next/image";
 import React, { useState } from "react";
 import HomeCard from "./HomeCard"; //creating reusable car, we use it four times
 import { useRouter } from "next/navigation";
+import MeetingModal from "./MeetingModal";
 
 const MeetingTypeList = () => {
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isInstantMeeting" | "isJoiningMeeting" | undefined
   >();
   const router = useRouter(); //??????????????????????????????????????
+
+  const createMeeting = () => {};
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Start an instant meeting"
-        handleClick={() => setMeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-orange-1"
       />
       <HomeCard
@@ -38,6 +41,15 @@ const MeetingTypeList = () => {
         description="Via Invitation Link"
         handleClick={() => setMeetingState("isJoiningMeeting")}
         className="bg-yellow-1"
+      />
+
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
       />
     </section>
   );
